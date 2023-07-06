e5a1e70cd2e3f62f1327318e300b624136c499cd
allyleandre <tuyambazeleandre@gmail.com>
2023-07-06 23:51:45 +0200

    learn about primitive variables

--- src/com/alicode/Main.java
+++ src/com/alicode/Main.java
@@ -2,14 +2,14 @@ package com.alicode;
 
 public class Main {
     public static void main(String[] args) {
-        // single line comment
-        /*
-           multi
-           line
-           comment
-         */
-       System.out.println("Hello Java");
-       System.out.println(10 + 10);
+    //Primitives
 
+        int number1 = 10;
+        int number2 = 20;
+        int results = number1 + number2;
+        System.out.println(results);
+        double pi = 3.14;
+        boolean isAdult = true;
+        char a = 'A';
     }
 }

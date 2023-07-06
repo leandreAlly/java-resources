1fbac37234eb9010ca5484e61ecb466816e88b85
allyleandre <tuyambazeleandre@gmail.com>
2023-07-06 23:44:51 +0200

    learn about comment

--- src/com/alicode/Main.java
+++ src/com/alicode/Main.java
@@ -2,6 +2,12 @@ package com.alicode;
 
 public class Main {
     public static void main(String[] args) {
+        // single line comment
+        /*
+           multi
+           line
+           comment
+         */
        System.out.println("Hello Java");
        System.out.println(10 + 10);
 

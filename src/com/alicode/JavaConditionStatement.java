64c94294c70b50cd61ffd34b2043a7043594f05b
allyleandre <tuyambazeleandre@gmail.com>
2023-07-10 07:02:14 +0200

    working with else if ladder condition

--- src/com/alicode/JavaConditionStatement.java
+++ src/com/alicode/JavaConditionStatement.java
@@ -2,11 +2,15 @@ package com.alicode;
 
 public class JavaConditionStatement {
    void checkAdult(){
-       int age = 18;
+       int age = 1;
        boolean isAdult = age >= 18;
 
        if(isAdult){
            System.out.println("You're an Adult :)");
+       }else if(age == 0) {
+           System.out.println("Baby :)");
+       }else if(age == 1) {
+           System.out.println("toddler :)");
        }else{
            System.out.println("Not an Adult :(");
        }
